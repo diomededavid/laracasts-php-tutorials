d59f675e368a12114a88c2790aa9ed7ab573ba6b
David Diomede <=>
2018-02-13 21:56:14 -0700

    Add head and footer files

---
+++ php-learning/php-forms/partials/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dave
+ * Date: 2/13/18
+ * Time: 9:39 PM
+ */
---
+++ php-learning/php-forms/partials/head.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dave
+ * Date: 2/13/18
+ * Time: 9:38 PM
+ */
