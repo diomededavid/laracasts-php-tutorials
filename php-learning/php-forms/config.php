3e3ab3e3f6d48969307718af08312cc09690e8b8
David Diomede <=>
2018-02-13 21:32:06 -0700

    Remove password

--- php-learning/php-forms/config.php
+++ php-learning/php-forms/config.php
@@ -10,7 +10,7 @@ return [
         'connection' => 'mysql:host = 127.0.0.1',
         'name'       => 'mytodo',
         'username'   => 'root',
-        'password'   => 'shadow',
+        'password'   => '',
         'options'   => [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]
 
     ]
